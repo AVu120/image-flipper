478950149f753abf89a0921247385d5bd56cd0fc
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-05-02 09:30:49 +1000

    Added code to enable rotation of all grid images (independant of each other).

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -5,10 +5,30 @@ import styles from "./grid.module.css";
 const Grid = ({ url }) => {
   return (
     <div className={styles.grid}>
-      <Image className={styles.grid__image} url={url} />
-      <Image className={styles.grid__image} url={url} />
-      <Image className={styles.grid__image} url={url} />
-      <Image className={styles.grid__image} url={url} />
+      <div
+        className={styles.grid__imageBorder}
+        style={{ "--rotation": `rotate(${0}deg)` }}
+      >
+        <Image className={styles.grid__image} url={url} />
+      </div>
+      <div
+        className={styles.grid__imageBorder}
+        style={{ "--rotation": `rotate(${0}deg)` }}
+      >
+        <Image className={styles.grid__image} url={url} />
+      </div>
+      <div
+        className={styles.grid__imageBorder}
+        style={{ "--rotation": `rotate(${0}deg)` }}
+      >
+        <Image className={styles.grid__image} url={url} />
+      </div>
+      <div
+        className={styles.grid__imageBorder}
+        style={{ "--rotation": `rotate(${0}deg)` }}
+      >
+        <Image className={styles.grid__image} url={url} />
+      </div>
     </div>
   );
 };
--- src/components/grid/image/image.js
+++ src/components/grid/image/image.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
-import styles from "./image.module.css";
 
 const Image = ({ className, url }) => {
-  const [position, setPosition] = useState("");
-
   return (
-    <div className={className}>
-      <img
-        src={url}
-        alt="No Image available. Please enter another image url below and click DISPLAY."
-      />
-    </div>
+    <img
+      src={url}
+      className={className}
+      alt="No Image available. Please enter another image url below and click DISPLAY."
+    />
   );
 };
 
