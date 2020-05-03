a2f1288247000eacd39d4d9dab9f640b227086ed
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-05-03 10:58:13 +1000

    Refactored grid.js to remove duplicate code via utilizaing multiple calls of generic function.

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Image from "./image/image";
+import * as style from "./grid.style";
 import styles from "./grid.module.css";
-
-const nonSelectedBorder = "3px solid black";
-const selectedBorder = "3px solid #f50057";
 
 const Grid = ({
   url,
@@ -13,24 +11,44 @@ const Grid = ({
   isHorizontallyFlipped,
   isVerticallyFlipped,
 }) => {
+  const flipHorizontally = (value) => {
+    return value ? `rotateY(180deg)` : `rotateY(0deg)`;
+  };
+  const flipVertically = (value) => {
+    return value ? `rotateX(180deg)` : `rotateX(0deg)`;
+  };
+
+  const modifyImage = (
+    degrees,
+    gridPosition,
+    horizontallyFlipped,
+    verticallyFlipped
+  ) => {
+    return {
+      "--rotation": `rotate(${degrees}deg)`,
+      border: `${
+        selectedImages.includes(gridPosition)
+          ? style.selectedBorder
+          : style.nonSelectedBorder
+      }`,
+      "--horizontalInversion": `${
+        horizontallyFlipped ? `rotateY(180deg)` : `rotateY(0deg)`
+      }`,
+      "--verticalInversion": `${
+        verticallyFlipped ? `rotateX(180deg)` : `rotateX(0deg)`
+      }`,
+    };
+  };
   return (
     <div className={styles.grid}>
       <div
         className={styles.grid__imageBorder}
-        style={{
-          "--rotation": `rotate(${rotation.topLeft}deg)`,
-          border: `${
-            selectedImages && selectedImages.includes("topLeft")
-              ? selectedBorder
-              : nonSelectedBorder
-          }`,
-          "--horizontalInversion": isHorizontallyFlipped.topLeft
-            ? `rotateY(180deg)`
-            : `rotateY(0deg)`,
-          "--verticalInversion": isVerticallyFlipped.topLeft
-            ? `rotateX(180deg)`
-            : `rotateX(0deg)`,
-        }}
+        style={modifyImage(
+          rotation.topLeft,
+          "topLeft",
+          isHorizontallyFlipped.topLeft,
+          isVerticallyFlipped.topLeft
+        )}
         onClick={() => changeSelectedImages("topLeft")}
       >
         <Image
@@ -40,60 +58,36 @@ const Grid = ({
       </div>
       <div
         className={styles.grid__imageBorder}
-        style={{
-          "--rotation": `rotate(${rotation.topRight}deg)`,
-          border: `${
-            selectedImages && selectedImages.includes("topRight")
-              ? selectedBorder
-              : nonSelectedBorder
-          }`,
-          "--horizontalInversion": isHorizontallyFlipped.topRight
-            ? `rotateY(180deg)`
-            : `rotateY(0deg)`,
-          "--verticalInversion": isVerticallyFlipped.topRight
-            ? `rotateX(180deg)`
-            : `rotateX(0deg)`,
-        }}
+        style={modifyImage(
+          rotation.topRight,
+          "topRight",
+          isHorizontallyFlipped.topRight,
+          isVerticallyFlipped.topRight
+        )}
         onClick={() => changeSelectedImages("topRight")}
       >
         <Image className={styles.grid__image} url={url} />
       </div>
       <div
         className={styles.grid__imageBorder}
-        style={{
-          "--rotation": `rotate(${rotation.bottomLeft}deg)`,
-          border: `${
-            selectedImages && selectedImages.includes("bottomLeft")
-              ? selectedBorder
-              : nonSelectedBorder
-          }`,
-          "--horizontalInversion": isHorizontallyFlipped.bottomLeft
-            ? `rotateY(180deg)`
-            : `rotateY(0deg)`,
-          "--verticalInversion": isVerticallyFlipped.bottomLeft
-            ? `rotateX(180deg)`
-            : `rotateX(0deg)`,
-        }}
+        style={modifyImage(
+          rotation.bottomLeft,
+          "bottomLeft",
+          isHorizontallyFlipped.bottomLeft,
+          isVerticallyFlipped.bottomLeft
+        )}
         onClick={() => changeSelectedImages("bottomLeft")}
       >
         <Image className={styles.grid__image} url={url} />
       </div>
       <div
         className={styles.grid__imageBorder}
-        style={{
-          "--rotation": `rotate(${rotation.bottomRight}deg)`,
-          border: `${
-            selectedImages && selectedImages.includes("bottomRight")
-              ? selectedBorder
-              : nonSelectedBorder
-          }`,
-          "--horizontalInversion": isHorizontallyFlipped.bottomRight
-            ? `rotateY(180deg)`
-            : `rotateY(0deg)`,
-          "--verticalInversion": isVerticallyFlipped.bottomRight
-            ? `rotateX(180deg)`
-            : `rotateX(0deg)`,
-        }}
+        style={modifyImage(
+          rotation.bottomRight,
+          "bottomRight",
+          isHorizontallyFlipped.bottomRight,
+          isVerticallyFlipped.bottomRight
+        )}
         onClick={() => changeSelectedImages("bottomRight")}
       >
         <Image className={styles.grid__image} url={url} />
---
+++ src/components/grid/grid.style.js
@@ -0,0 +1,2 @@
+export const nonSelectedBorder = "3px solid black";
+export const selectedBorder = "3px solid #f50057";
