a3f07a1defbc69274c5d67a69fda126f42c0338b
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-05-26 07:05:45 +1000

    Updates

---
+++ precache-manifest.906dcfbe9308c0058f467a5d805f6fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f2789de909b7dc6325b685491b6812",
+    "url": "/image-flipper/index.html"
+  },
+  {
+    "revision": "d97e9b936e69597363b1",
+    "url": "/image-flipper/static/css/main.5583f6e8.chunk.css"
+  },
+  {
+    "revision": "c2768e645e8d9839b746",
+    "url": "/image-flipper/static/js/2.28ffd0f3.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/image-flipper/static/js/2.28ffd0f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97e9b936e69597363b1",
+    "url": "/image-flipper/static/js/main.f75aa76f.chunk.js"
+  },
+  {
+    "revision": "833cbec25c582d255538",
+    "url": "/image-flipper/static/js/runtime-main.49969496.js"
+  }
+]);
